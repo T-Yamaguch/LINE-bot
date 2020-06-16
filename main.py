3c6c7e301078ee51252886493574b70c81b469fa
T-Yamaguch <64463679+T-Yamaguch@users.noreply.github.com>
2020-06-16 14:14:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -84,23 +84,23 @@ def face_detect(image):
     if len(face) > 0:
         #顔部分を切り取る
         x,y,w,h = face[0]
-        if y > 0.25*h:
-            y_o =int(y-0.25*h)
+        if y > h:
+            y_o =y-h
         else:
             y_o =0
 
-        if  y+1.75*h < image.shape[0]:
-            y_h = int(y+1.75*h)
+        if  y+2*h < image.shape[0]:
+            y_h = y+2*h
         else:
             y_h = image.shape[0]
         
-        if x > 0.5*w:
-            x_o =int(x-0.5*w)
+        if x > w:
+            x_o =x-w
         else:
             x_o =0
         
-        if x+1.5*w < image.shape[1]:
-            x_w = int(x+1.5*w)
+        if x+2*w < image.shape[1]:
+            x_w = x+2*w
         else:
             x_w = image.shape[1]
 
